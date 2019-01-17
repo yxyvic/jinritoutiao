d719427c0a9317758819829a08515c626dcd7e50
yxy <yxynbnb@qq.com>
2019-01-17 17:45:11 +0800

    build config

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: './',  //修改打包路径  '/'--->'./'
 
     /**
      * Source Maps
